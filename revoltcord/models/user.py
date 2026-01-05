d6b47915c4bc372f74b2ecdd2ec956918736ac68
John/Dev <jgarland13191@gmail.com>
2026-01-04 22:24:37 -0500

    Update user.py

--- revoltcord/models/user.py
+++ revoltcord/models/user.py
@@ -4,6 +4,10 @@ Represents a Revolt user.
 """
 
 class User:
-    def __init__(self, id: str, username: str):
+    def __init__(self, id: str, username: str, avatar: str | None = None):
         self.id = id
         self.username = username
+        self.avatar = avatar
+
+    def __repr__(self):
+        return f"<User id={self.id!r} username={self.username!r}>"
