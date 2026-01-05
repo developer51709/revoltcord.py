7c5cb8207a25880d8e914c203c29bfd65b5af8a0
John/Dev <jgarland13191@gmail.com>
2026-01-04 22:13:46 -0500

    Update message.py

--- revoltcord/models/message.py
+++ revoltcord/models/message.py
@@ -1,2 +1,17 @@
+"""
+Message model
+Represents a message sent in a Revolt channel.
+"""
+
 class Message:
-    pass
+    def __init__(self, id: str, content: str, author, channel):
+        self.id = id
+        self.content = content
+        self.author = author
+        self.channel = channel
+
+    async def edit(self, content: str):
+        raise NotImplementedError("Message.edit() has not been implemented yet.")
+
+    async def delete(self):
+        raise NotImplementedError("Message.delete() has not been implemented yet.")
