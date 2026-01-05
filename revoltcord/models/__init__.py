6c0c61c2b5337a06463687b00609971836bf0c8e
John/Dev <jgarland13191@gmail.com>
2026-01-04 22:12:56 -0500

    Update __init__.py

--- revoltcord/models/__init__.py
+++ revoltcord/models/__init__.py
@@ -1,1 +1,8 @@
+"""
+Model package for Revolt objects such as Message, User, Channel, and Server.
+"""
 
+from .message import Message
+from .user import User
+from .channel import Channel
+from .server import Server
